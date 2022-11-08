3760a73b8793a1ee29dd4a185490bf615b4a66fe
Giedrius <49316774+Longas13@users.noreply.github.com>
2022-11-08 12:10:56 +0200

    feat: catch routing errors

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,4 +1,7 @@
-import { useRouteError } from "react-router-dom";
+import { Box } from "@mui/system";
+import { Button } from "cx-portal-shared-components";
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
 //TODO: Create Better Layout
 
 export default function ErrorPage() {
@@ -6,12 +9,48 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <section>
+        <Box className="irs-error-page">
+          <Box className="irs-error-page-header">
+            <h1>Oops!</h1>
+          </Box>
+          <Box className="error-page-body">
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+              <i>{error.statusText || error.message}</i>
+            </p>
+            <Link to="/">
+              <Button style={{ margin: 20 }} variant="contained" color="secondary">
+                Go Home
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+      </section>
+    </>
   );
 }
+
+export const ErrorDisplay: React.FC<{ error: unknown }> = ({ error }) => {
+  return (
+    <section>
+      <Box className="irs-error-page">
+        <Box className="irs-error-page-header">
+          <h1>Oops!</h1>
+        </Box>
+        <Box className="error-page-body">
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <i>{(error as Error).message}</i>
+          </p>
+          <Link to="/">
+            <Button style={{ margin: 20 }} variant="contained" color="secondary">
+              Go Home
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </section>
+  );
+};
